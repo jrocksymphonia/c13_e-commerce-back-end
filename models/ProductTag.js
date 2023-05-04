c2e902779926b9af9e8b9b2e7051fb7502873a8c
jrocksymphonia <122930110+jrocksymphonia@users.noreply.github.com>
2023-05-04 11:54:04 -0400

    commented the prepped model content for those tables

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,20 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+
+  // * `id`
+  //   * Integer.
+  //   * Doesn't allow null values.
+  //   * Set as primary key.
+  //   * Uses auto increment.
+
+  // * `product_id`
+  //   * Integer.
+  //   * References the `Product` model's `id`.
+
+  // * `tag_id`
+  //   * Integer.
+  //   * References the `Tag` model's `id`.
   },
   {
     sequelize,
