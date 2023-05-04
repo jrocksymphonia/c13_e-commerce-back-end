3263944daee611ac65bb1b89087e7a0a866882d6
jrocksymphonia <122930110+jrocksymphonia@users.noreply.github.com>
2023-05-04 12:52:52 -0400

    Product.js model is done

--- models/Product.js
+++ models/Product.js
@@ -11,30 +11,43 @@ Product.init(
   {
     // define columns
 
-  // * `id`
-  //   * Integer.
-  //   * Doesn't allow null values.
-  //   * Set as primary key.
-  //   * Uses auto increment.
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
 
-  // * `product_name`
-  //   * String.
-  //   * Doesn't allow null values
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
 
-  // * `price`
-  //   * Decimal.
-  //   * Doesn't allow null values.
-  //   * Validates that the value is a decimal.
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
 
-  // * `stock`
-  //   * Integer.
-  //   * Doesn't allow null values.
-  //   * Set a default value of `10`.
-  //   * Validates that the value is numeric.
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
 
-  // * `category_id`
-  //   * Integer.
-  //   * References the `Category` model's `id`.
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+        unique: false
+      }
+    },
   },
   {
     sequelize,
