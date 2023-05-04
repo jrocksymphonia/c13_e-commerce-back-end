8766f0a6b4bbe8284b92ce0744a9214d04bcdab1
jrocksymphonia <122930110+jrocksymphonia@users.noreply.github.com>
2023-05-04 12:37:31 -0400

    Tag.js model is done

--- models/Tag.js
+++ models/Tag.js
@@ -8,14 +8,15 @@ Tag.init(
   {
     // define columns
 
-  // * `id`
-  //   * Integer.
-  //   * Doesn't allow null values.  
-  //   * Set as primary key.
-  //   * Uses auto increment.
-
-  // * `tag_name`
-  //   * String.
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    }
   },
   {
     sequelize,
